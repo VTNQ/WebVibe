30f14b8a9a9dc5d7f1d9fac273f3b26425a3dd00
VTNQ <votannamquoc@gmail.com>
2025-05-25 11:35:52 +0700

    Update Section 6,7 by VTNQ

--- fe/src/pages/Homepages.tsx
+++ fe/src/pages/Homepages.tsx
@@ -4,12 +4,14 @@ import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Section8 from "../components/Homepage/Section8/Section8";
 import Section3 from "../components/Homepage/Section3/Section3";
+import Section6 from "../components/Homepage/Section6/Section6";
+import Section7 from "../components/Homepage/Section7/Section7";
 const HomePage=()=>{
   
 return(
     <>
     <NavBar/>
-    <Section3/>
+    <Section3/><Section6/><Section7/>
     <Section8/>
     <Footer/>
     </>
